452c77538142695e28a4e3a5caac7cc2889edc33
vanshraj <vanshajbehl96@gmail.com>
2017-06-06 16:32:06 +0530

    added simulation jar file to run from index.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ThinkQuant' });
+	
+	var exec = require('child_process').exec;
+	var child = exec('java -jar ./public/simulation.jar',function (error, stdout, stderr){
+	    console.log('Output -> ' + stdout);
+	    if(error !== null){
+	    	console.log("Error -> "+error);
+	    }
+	});
+	 
+	module.exports = child;
+	res.render('index', { title: 'ThinkQuant' });
 });
 
 module.exports = router;
