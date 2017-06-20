21a867bd3e015a7497368dcda0530d2dd3d4bd10
vanshraj <vanshajbehl96@gmail.com>
2017-06-20 17:24:17 +0530

    added sitemap xml

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
 	res.render('index/index', { title: 'ThinkQuant' });
 });
 
+
 //about route
 router.get('/about',function(req,res){
 	res.render('index/about',{
