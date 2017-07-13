aced66999cfdf90e64f49f3aab9935317cb2df3b
vanshraj <vanshajbehl96@gmail.com>
2017-07-13 14:45:42 +0530

    added java cmd

--- routes/admin.js
+++ routes/admin.js
@@ -2,26 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/start',function(req, res, next){
-	res.send("start");
-	// var exec = require('child_process').exec; 
-	// var child = exec('java -jar ./public/simulation.jar',function (error, stdout, stderr){ 
-	// console.log('Output -> ' + stdout); 
-	// if(error !== null){
-	// 	console.log("Error -> "+error); } 
-	// });
-	// module.exports = child; 
+	// res.send("start");
+	var exec = require('child_process').exec; 
+	var child = exec('java -jar ../Simulation/simulation.jar',function (error, stdout, stderr){ 
+	console.log('Output -> ' + stdout); 
+	if(error !== null){
+		console.log("Error -> "+error); } 
+	});
+	module.exports = child; 
 });
 
 router.get('/stop',function(req, res, next){
-	res.send("stop");	
+	var exec = require('child_process').exec; 
+	var child = exec('',function (error, stdout, stderr){ 
+	console.log('Output -> ' + stdout); 
+	if(error !== null){
+		console.log("Error -> "+error); } 
+	});
+	module.exports = child; 	
 });
 
 router.get('/live',function(req, res, next){
-	res.send("live");
+	var exec = require('child_process').exec; 
+	var child = exec('python ../Simulation/dump_live.py',function (error, stdout, stderr){ 
+	console.log('Output -> ' + stdout); 
+	if(error !== null){
+		console.log("Error -> "+error); } 
+	});
+	module.exports = child; 
 });
 
 router.get('/prepare',function(req, res, next){
-	res.send("prepare");	
+	var exec = require('child_process').exec; 
+	var child = exec('java -jar ../Simulation/Prepare.jar',function (error, stdout, stderr){ 
+	console.log('Output -> ' + stdout); 
+	if(error !== null){
+		console.log("Error -> "+error); } 
+	});
+	module.exports = child; 	
 });
 
 
