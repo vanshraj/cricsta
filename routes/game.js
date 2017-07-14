52b4d53daea5a96bd7f10606597b5e95ded90ab1
vanshraj <vanshajbehl96@gmail.com>
2017-07-14 16:17:29 +0530

    slots section made and player sorted name

--- routes/game.js
+++ routes/game.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Match = require('../models/match');
 var Player = require('../models/match');
+var _ = require('lodash');
 
 router.get('/',function(req, res){
 	Match.getMatchLatestData(function(err,match){
 		if(err) throw err;
 		Player.getPlayerData(match,function(err,data){
 			if(err) throw err;
-			Match.getMatchFirstProb(match, function(err,doc){
-				if(err) throw err;
-				res.render('game/table',{ data:data, doc:doc, title:"Game"});
-			});
+			var team1players = _.unionBy( data.team1.bowlers,data.team1.batsmen, 'name');
+			team1players = _.sortBy(team1players,'name');
+			var team2players = _.unionBy( data.team2.bowlers,data.team2.batsmen,'name');
+			team2players = _.sortBy(team2players,'name');
+			res.render('game/table',{ data:data, team2players:team2players, team1players:team1players, title:"Game"});
 		});
 	});
 });
