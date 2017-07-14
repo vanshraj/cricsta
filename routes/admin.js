a68078e5c3165aac1b09daf6eddaa6205cfd287b
Ubuntu <vanshajbehl96@gmail.com>
2017-07-14 07:08:43 +0000

    start stop done

--- routes/admin.js
+++ routes/admin.js
@@ -1,61 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process').exec;
 
 var childpid="";
+var pythonpid="";
 
 router.get('/start',isAdmin, function(req, res, next){
 	if(childpid==""){
-		var exec = require('child_process').exec; 
-		var child = exec('cd ../Simulation && java -jar Simulation.jar',function (error, stdout, stderr){ 
-		console.log('Output -> ' + stdout);
-		if(error !== null){
-			console.log("Error -> "+error); 
-			res.send("error occured");
-			} 
-		});
-		childpid=child.pid;
+		var child = exec('cd ../Simulation && java -jar Simulation.jar');
+		var child2 = exec('cd ../Simulation && python dump_live.py');
+		childpid=child.pid+2;
+		pythonpid=child2.pid+2;
+		req.flash('success','process started.');
 		res.redirect('/users/account');
 	}else{
 		req.flash('error','Already running simulation stop first');
-    	res.redirect('/users/account');
+	    	res.redirect('/users/account');
 	}
 });
 
 router.get('/stop',isAdmin, function(req, res, next){
 	if(childpid==""){
-		req.flash('error','There is no child');
-    	res.redirect('/users/account');
+		req.flash('error','There is no process started.');
+    		res.redirect('/users/account');
 	}else{
-		var exec = require('child_process').exec; 
-		var child = exec('kill '+childpid,function (error, stdout, stderr){
-		childpid="" 
-		console.log('Output -> ' + stdout);
-		if(error !== null){
-			console.log("Error -> "+error); 
-			res.send("error occured");
-			} 
+		console.log(childpid);
+		var child = exec('kill '+childpid, function(){
+			childpid="";
+			var child2 = exec('kill '+pythonpid, function(){
+				pythonpid="";
+				req.flash('success','process killed.');
+				res.redirect('/users/account');
+			});	
 		});
-		res.redirect('/users/account');
 	}
 });
 
-router.get('/live',isAdmin, function(req, res, next){
-	var exec = require('child_process').exec; 
-	var child = exec('cd ../Simulation && python dump_live.py',function (error, stdout, stderr){ 
-	console.log('Output -> ' + stdout); 
-	if(error !== null){
-		console.log("Error -> "+error); } 
-	});
-	res.redirect('/users/account');
-});
-
 router.get('/prepare',isAdmin, function(req, res, next){
-	var exec = require('child_process').exec; 
-	var child = exec('cd ../Simulation && java -jar Prepare.jar',function (error, stdout, stderr){ 
-	console.log('Output -> ' + stdout); 
-	if(error !== null){
-		console.log("Error -> "+error); } 
+	var child = exec('cd ../Simulation && java -jar Prepare.jar',function(error,stdout){
+		console.log("output - > "+stdout);
 	});
+	req.flash('success','prepare started.');
 	res.redirect('/users/account');	
 });
 
