2213c1561af45ae33c3cbeeb2ea41ac46a8e3bbf
vanshraj <vanshajbehl96@gmail.com>
2017-07-17 14:41:38 +0530

    added modal in buy and sell

---
+++ public/javascripts/game.js
@@ -0,0 +1,74 @@
+//modal open
+$('.buyButton').click(function(){
+  var content = $(this).parent().parent().children(':first-child').html();
+  var points = $(this).next().text();
+  points =points.slice(0, -7);
+  var quantity =$('input[name="b-quantity"]').val();
+  $('.mini.buying.modal .header').html(content);
+  $('.mini.buying.modal .description1').text('Total Points Needed : '+ (Math.round(points*quantity* 100) / 100) );
+  $('.mini.buying.modal .bPrice input').val(points);
+  $('.mini.buying.modal')
+  .modal('show')
+  ;  
+});
+
+$('.sellButton').click(function(){
+  var content = $(this).parent().parent().children(':first-child').html();
+  var points = $(this).prev().text();
+  points =points.slice(0, -7);
+  var quantity =$('input[name="s-quantity"]').val();
+  $('.mini.selling.modal .header').html(content);
+  $('.mini.selling.modal .description1').text('Total Points Needed : '+ (Math.round(points*quantity* 100) / 100) );
+  $('.mini.selling.modal .sPrice input').val(points);
+  $('.modal.selling')
+  .modal('show')
+  ;  
+});
+
+
+//plus minus in modal
+$('.plusButton').click(function(e){
+            e.preventDefault();
+            fieldName = $(this).attr('field');
+
+            var category = fieldName.slice(0,2);
+
+            var currentVal = parseInt($('input[name='+fieldName+']').val());
+            if (!isNaN(currentVal)) {
+
+              $('input[name='+fieldName+']').val(currentVal + 1);
+
+              var quantity =$('input[name='+category+'quantity'+']').val();
+              var price =$('input[name='+category+'price'+']').val();
+              $(this).parent().parent().parent().parent().children('.description1').text('Total Points Needed : '+ (Math.round(price*quantity* 100) / 100) );
+
+            } else {
+              $('input[name='+fieldName+']').val(1);
+
+              var quantity =$('input[name='+category+'quantity'+']').val();
+              var price =$('input[name='+category+'price'+']').val();
+              $(this).parent().parent().parent().parent().children('.description1').text('Total Points Needed : '+ (Math.round(price*quantity* 100) / 100) );
+            }
+        });
+$('.minusButton').click(function(e){
+            e.preventDefault();
+            fieldName = $(this).attr('field');
+
+            var category = fieldName.slice(0,2);
+
+            var currentVal = parseInt($('input[name='+fieldName+']').val());
+            if (!isNaN(currentVal)) {
+              $('input[name='+fieldName+']').val(currentVal - 1);
+
+              var quantity =$('input[name='+category+'quantity'+']').val();
+              var price =$('input[name='+category+'price'+']').val();
+              $(this).parent().parent().parent().parent().children('.description1').text('Total Points Needed : '+ (Math.round(price*quantity* 100) / 100) );
+
+            } else {
+              $('input[name='+fieldName+']').val(1);
+
+              var quantity =$('input[name='+category+'quantity'+']').val();
+              var price =$('input[name='+category+'price'+']').val();
+              $(this).parent().parent().parent().parent().children('.description1').text('Total Points Needed : '+ (Math.round(price*quantity* 100) / 100) );
+            }
+        });
